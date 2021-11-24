df8aeea1265605c4c618938af5005c7ccd833f1a
1117070 <94019451+1117070@users.noreply.github.com>
2021-11-24 14:37:11 -0800

    Add files via upload

---
+++ GitHub/Variables/src/expressions.java
@@ -0,0 +1,44 @@
+public class expressions {
+    public static void main( String[] args )
+    {
+        // expressions 1
+      //  System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
+      //  System.out.println((9 - 4));
+      //  System.out.println((4 - 8));
+      //  System.out.println((4 - 8)/3);
+      //  System.out.println(10/2);
+      //  System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
+
+        // expressions 2
+      //   System.out.println(3 / 2);
+      //  System.out.println(18 - 1);
+      //  System.out.println(23%4);
+      //  System.out.println(3 + 17);
+      //  System.out.println(23%4 + (18 - 3 / 2));
+
+        // expressions 3
+      //  System.out.println(((8 - 5) + Math.pow(3,4)/5));
+      //  System.out.println(Math.pow(3,4));
+      //  System.out.println((81/5));
+      //  System.out.println((8 - 5));
+      //  System.out.println(3 + 16);
+
+        // expressions 4
+      //  System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+      //  System.out.println((9 % 4));
+      //  System.out.println(Math.pow(2,3));
+      //  System.out.println((Math.sqrt(81)));
+      //  System.out.println(9 / 3);
+      //  System.out.println(8 / 8);
+      //  System.out.println(1 - 8 + 1 + 3);
+
+        // expressions 5
+      //  System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+      //  System.out.println(14 / 2);
+      //  System.out.println(Math.sqrt(25));
+      //  System.out.println(Math.pow(5,3));
+      //  System.out.println(Math.sin(45));
+      //  System.out.println((10 * 0.8509035245341184 + 2));
+      //  System.out.println(10.509035245341185 - 5);
+    }
+}
---
+++ GitHub/Variables/src/variableprac.java
@@ -0,0 +1,13 @@
+public class variableprac {
+    public static void main( String[] args )
+    {
+
+        int first, second, third;
+
+        double fourth, fifth;
+
+
+
+
+    }
+}
